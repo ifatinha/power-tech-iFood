b28311b2aa6feafcd23e869a8208947859213525
ifatinha <ifatinha99@gmail.com>
2023-11-22 10:48:42 -0300

    Mapeando teclas permitidas

--- piano-simulator/assets/js/engine.js
+++ piano-simulator/assets/js/engine.js
@@ -1,18 +1,35 @@
 function piano() {
     const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+    const mapedKeys = []
+
     function newAudio(name, type = "wav") {
         return new Audio(`./assets/audios/${name}.${type}`);
     }
 
     function playTune(key) {
         newAudio(key).play();
+
+        const clickedKey = document.querySelector(`[data-key='${key}']`);
+        clickedKey.classList.add("active");
+
+        setTimeout(() => {
+            clickedKey.classList.remove("active");
+        }, 150);
     }
 
     pianoKeys.forEach((key) => {
+        mapedKeys.push(key.dataset.key);
         key.addEventListener("click", () => {
             playTune(key.dataset.key);
         });
+    })
+
+    document.addEventListener("keydown", (e) => {
+        if (mapedKeys.includes(e.key)) {
+            console.log("Aqui estou!");
+            playTune(e.key);
+        }
     })
 }
 
