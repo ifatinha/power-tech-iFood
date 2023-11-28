40bf30f3a60717c138e4e5b642f09d0b63c622c2
ifatinha <ifatinha99@gmail.com>
2023-11-28 09:06:46 -0300

    Criando enumeracao das cartas

--- game-yu/assets/js/engine.js
+++ game-yu/assets/js/engine.js
@@ -21,10 +21,43 @@ const state = {
     }
 }
 
+const pathImages = "./../icons/"
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        winOf: [1],
+        loseOf: [2]
+    },
+
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        winOf: [2],
+        loseOf: [0]
+    },
+
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        winOf: [0],
+        loseOf: [1]
+    }
+
+]
+
 function init() {
 
 }
 
 (() => {
     init();
+    console.log(cardData[0].img);
 })()
